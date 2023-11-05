cd54c4fa2783d98d693047044b35d5a2d26c62ce
faizan <xamarindeveloper@icloud.com>
2023-11-05 05:27:24 -0500

    source code

---
+++ maui/UnityActivity.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Com.Unity3d.Player;
+
+namespace maui
+{
+    [Activity(
+        Label = "UnityActivity",
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+        ScreenOrientation = ScreenOrientation.FullUser,
+        Theme = "@android:style/Theme.NoTitleBar")]
+    public class UnityActivity : Activity
+    {
+        private UnityPlayer unityPlayer;
+
+        public UnityActivity()
+        {
+
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            unityPlayer = new UnityPlayer(this);
+            SetContentView(unityPlayer);
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            unityPlayer.Pause();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            unityPlayer.Resume();
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            unityPlayer.Quit();
+        }
+
+    }
+}
+
